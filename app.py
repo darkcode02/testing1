306710aaaeaed45d15034b679316c9de7e5f6ccf
darkcode0 <tupabloarias12345@gmail.com>
2024-11-21 19:29:02 -0500

    cambios

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-def recorrer_lista(lista1, dato): 
-    for i in lista1: 
-        lista1.append(dato)
-    return lista1
-
-resultado_lista = recorrer_lista([], "juan pablo")
-
-print(resultado_lista)
+print(nuevo cambio)
